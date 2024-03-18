1152950f969d7b0f4891f35c410720811b0bc987
Travis Jones <toj817@gmail.com>
2024-03-18 03:48:31 -0700

    Adding index page

---
+++ views/places/index.jsx
@@ -0,0 +1,13 @@
+const React = require('react')
+const Def = require('./default')
+
+function index () {
+    return (
+      <Def>
+          <main>
+              <h1>INDEX</h1>
+          </main>
+      </Def>
+    )
+  }
+  
