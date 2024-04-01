8c6e820a0ab0c87305332b33c9694137d1f1a18d
Travis Jones <toj817@gmail.com>
2024-04-01 13:09:46 -0700

    Jazzed up form page

--- views/places/new.jsx
+++ views/places/new.jsx
@@ -6,6 +6,29 @@ return (
     <Def>
         <main>
             <h1> Add a New Place</h1>
+            <form method="POST" action="/places">
+                <div className="form-group">
+                    <label htmlFor="name"> Place Name</label>
+                    <input id="name" name="name" required />
+                </div>
+                <div className="form-group">
+                     <label htmlFor="pic">Place Picture</label>
+                       <input type="url" id="pic" name="pic" />
+                       </div>
+                <div className="form-group">
+                    <label htmlFor="city"> City</label>
+                    <input id = "city" name= "city"/>
+                </div>
+                <div className="form-group">
+                    <label htmlFor="state"> State</label>
+                    <input id="state" name="state" />
+                </div>
+                <div className="form-group"> 
+                    <label htmlFor="cuisines"> Cuisines</label>
+                    <input id="cuisines" name="cuisines" required />
+                </div>
+                <input className="btm btn-primary" type="submit" value="Add Place" />
+            </form>
         </main>
     </Def>
 )
